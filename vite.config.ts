490568c7dde38cfc1c430bc2dcfe22d9ae85abd4
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-10-01 15:59:29 +0500

    creating a compact build

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
             "@radix-ui/react-toast",
             "@radix-ui/react-tooltip",
           ],
+          table: ["@tanstack/react-table", "@xyflow/react"],
         },
       },
     },
