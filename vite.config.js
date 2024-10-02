9bdb6c465b696321c2dfa604bba5f417532e20fb
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2024-10-02 06:01:24 +0500

    Adjusting vite settings

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    react: ["react", "react-dom"],
+                    router: ["react-router-dom"],
+                    ui: [
+                        "@radix-ui/react-accordion",
+                        "@radix-ui/react-avatar",
+                        "@radix-ui/react-dialog",
+                        "@radix-ui/react-dropdown-menu",
+                        "@radix-ui/react-icons",
+                        "@radix-ui/react-label",
+                        "@radix-ui/react-popover",
+                        "@radix-ui/react-select",
+                        "@radix-ui/react-slot",
+                        "@radix-ui/react-toast",
+                        "@radix-ui/react-tooltip",
+                    ],
+                    table: ["@tanstack/react-table", "@xyflow/react"],
+                },
+            },
+        },
+        assetsInlineLimit: 0,
+        chunkSizeWarningLimit: 1500,
+        sourcemap: true,
+    },
+});
